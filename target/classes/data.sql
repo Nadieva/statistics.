8778a52f83c3cde34d03b70ead18c9f7bbf541c9
Nadia MM <44876058+Nadieva@users.noreply.github.com>
2023-09-26 00:46:57 +0200

    deleted the target folder

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO CUSTOMERS VALUES (1,'Big News Media Corp',true),(2,'Online Mega Store',true),(3,'Nachoroo Delivery',false),(4,'Euro Telecom Group',true);
-INSERT INTO IP_BLACKLIST VALUES (0),(2130706433),(4294967295);
-INSERT INTO USER_AGENT_BLACKLIST VALUES ('A6-Indexer'),('Googlebot-News'),('Googlebot');	
